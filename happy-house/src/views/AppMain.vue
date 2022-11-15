582c5a120bdf78799828f3c1b630785bd236e408
사공지은 <gonii0821@naver.com>
2022-11-15 14:59:43 +0900

    [Add] 메인페이지 하단 콘텐츠 컴포넌트 구현 및 폰트어썸 컴포넌트 구현

--- happy-house/src/views/AppMain.vue
+++ happy-house/src/views/AppMain.vue
@@ -1,14 +1,19 @@
 <template>
-  <MainSearch />
+  <div>
+    <MainSearch />
+    <MainContents />
+  </div>
 </template>
 
 <script type="module">
 import MainSearch from "@/components/main/MainSearch.vue";
+import MainContents from "@/components/main/MainContents.vue";
 
 export default {
   name: "AppMain",
   components: {
     MainSearch,
+    MainContents,
   },
 };
 </script>
