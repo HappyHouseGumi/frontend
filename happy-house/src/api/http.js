173d2842838c585a7ec19c5e3b737e4bf96c5278
사공지은 <gonii0821@naver.com>
2022-11-17 21:27:14 +0900

    [Fix] 이메일형식 맞게 입력받기

--- happy-house/src/api/http.js
+++ happy-house/src/api/http.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: "http://192.168.31.53/happy", // 학교
-  // baseURL: "http://218.150.0.132/happy", // 집
+  // baseURL: "http://192.168.31.53/happy", // 학교
+  baseURL: "http://218.150.0.132/happy", // 집
   headers: {
     "Content-Type": "application/json;charset=utf-8",
   },
