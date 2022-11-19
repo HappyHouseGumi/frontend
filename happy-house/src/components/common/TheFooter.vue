8fe9030c4ad75eca54e12ca80eee1a8b3172ffd1
사공지은 <gonii0821@naver.com>
2022-11-19 17:13:33 +0900

    [Modify] fixed footer로 레이아웃 수정

--- happy-house/src/components/common/TheFooter.vue
+++ happy-house/src/components/common/TheFooter.vue
@@ -2,18 +2,10 @@
   <footer>
     <div class="wrapper">
       <a href="https://github.com/HappyHouseGumi" target="_blank"
-        ><font-awesome-icon
-          icon="fa-brands fa-github"
-          class="fa-lg"
-          style="color: black"
+        ><font-awesome-icon icon="fa-brands fa-github" class="fa-lg" style="color: black"
       /></a>
-      <a
-        href="https://www.notion.so/ssafys/6d4a924e1b7543849596d770bf10f6f2"
-        target="_blank"
-        ><font-awesome-icon
-          icon="fa-solid fa-book"
-          class="fa-lg"
-          style="color: black"
+      <a href="https://www.notion.so/ssafys/6d4a924e1b7543849596d770bf10f6f2" target="_blank"
+        ><font-awesome-icon icon="fa-solid fa-book" class="fa-lg" style="color: black"
       /></a>
       <span>&copy; HappyHouse | 박소연 | 사공지은 | 성광현</span>
     </div>
@@ -28,11 +20,14 @@ export default {
 
 <style scoped>
 footer {
+  width: 100%;
   height: 80px;
-  position: relative;
-  transform: translateY(-100%);
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   border-top: 1px solid #eee;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  background: white;
 }
 
 .wrapper {
