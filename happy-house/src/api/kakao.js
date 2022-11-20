c1dc49e41f013129d877682f57ee3a36d52b2a3e
성광현 <gch03944@gmail.com>
2022-11-21 00:47:05 +0900

    [Add] kakao maker click

--- happy-house/src/api/kakao.js
+++ happy-house/src/api/kakao.js
@@ -6,13 +6,32 @@ const kakaoapi = kakaoInstance();
 
 function getKakaoLocation(location, success, fail) {
   kakaoapi
-    .get(`/v2/local/search/address.json?query=${encodeURIComponent(location)}`, {
-      headers: {
-        Authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_REST_API_KEY}`,
-      },
-    })
+    .get(
+      `/v2/local/search/address.json?query=${encodeURIComponent(location)}`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_REST_API_KEY}`,
+        },
+      }
+    )
     .then(success)
     .catch(fail);
 }
-
-export { getKakaoLocation };
+function getFindLocation(info, success, fail) {
+  kakaoapi
+    .get(
+      `/v2/local/search/category.json?category_group_code=${encodeURIComponent(
+        info.category
+      )}&x=${encodeURIComponent(info.lng)}&y=${encodeURIComponent(
+        info.lat
+      )}&radius=500`,
+      {
+        headers: {
+          Authorization: `KakaoAK ${process.env.VUE_APP_KAKAO_REST_API_KEY}`,
+        },
+      }
+    )
+    .then(success)
+    .catch(fail);
+}
+export { getKakaoLocation, getFindLocation };
