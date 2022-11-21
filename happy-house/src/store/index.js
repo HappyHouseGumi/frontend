59cb99bd73fb294d1d669dd7e779116e5ba33344
박소연 <soyeon_9605@yu.ac.kr>
2022-11-22 01:58:15 +0900

    [Add] Board Paging 미완성

--- happy-house/src/store/index.js
+++ happy-house/src/store/index.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import Vuex from "vuex";
 import userStore from "@/store/modules/userStore";
 import aptStore from "@/store/modules/aptStore";
+import boardStore from "@/store/modules/boardStore";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { userStore, aptStore },
+  modules: { userStore, aptStore, boardStore },
 });
